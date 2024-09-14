28921f71beb8fc79b45f1edaa2130b19b5750e20
Daniel (Daner) <danedo20472@gmail.com>
2024-09-14 18:21:45 -0500

    Added avatar

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,16 @@ import ExperienceCard from './components/ExperienceCard'
 
 function App() {
 
-  const Daniel = {
-    role: 'Student',
-    age: 21,
-    gender: 'Male',
-    status: 'Available'
-  }
-
 
   return (
     <>
       <section id='hero'>
         <div className="container">
-          <h1>Hola, soy Daniel</h1>
-          <p>Estudiante de <strong>Informática y Desarrollo de Aplicaciones Web.</strong> Apasionado por la tecnología. Conocimiento en HTML, CSS y JS. Frameworks como Angular y React.</p>
-          <code>
-            
-          </code>
+          <div className='description'>
+            <h1>Hola, soy Daniel</h1>
+            <p>Estudiante de <strong>Informática y Desarrollo de Aplicaciones Web.</strong> Apasionado por la tecnología. Conocimiento en HTML, CSS y JS. Frameworks como Angular y React.</p>
+          </div>
+          <img src="/img/pf-picture.jpeg" alt="Avatar Image" className='avatar' />
         </div>
       </section>
 
