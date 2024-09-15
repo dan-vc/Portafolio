824893810a1318b94b6a6982bafcabf50d752b84
Daniel (Daner) <danedo20472@gmail.com>
2024-09-14 19:50:20 -0500

    Added proyects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import './App.css'
 import ExperienceCard from './components/ExperienceCard'
+import CSS from './components/icons/Css'
+import HTML5 from './components/icons/html5'
+
+
 
 function App() {
 
@@ -32,6 +36,46 @@ function App() {
             time=''
             description='...'
           />
+        </div>
+      </section>
+
+      <section id='proyectos'>
+        <div className="container">
+          <h2>Proyectos</h2>
+          <div className="proyect-card">
+            <img src="/img/Placeholder-Image.jpg" alt="" />
+            <div className="content">
+              <h3>Titulo del proyecto</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit accusamus, et doloremque sequi fugiat illum unde delectus recusandae necessitatibus, sunt corporis commodi enim ipsam, quam est architecto numquam? Laborum, reprehenderit.</p>
+              <div className="tags">
+                <span className='tag-item'>
+                  <HTML5 />
+                  HTML
+                </span>
+                <span className='tag-item'>
+                  <CSS />
+                  CSS
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="proyect-card">
+            <img src="/img/Placeholder-Image.jpg" alt="" />
+            <div className="content">
+              <h3>Titulo del proyecto</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit accusamus, et doloremque sequi fugiat illum unde delectus recusandae necessitatibus, sunt corporis commodi enim ipsam, quam est architecto numquam? Laborum, reprehenderit.</p>
+              <div className="tags">
+                <span className='tag-item'>
+                  <HTML5 />
+                  HTML
+                </span>
+                <span className='tag-item'>
+                  <CSS />
+                  CSS
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
