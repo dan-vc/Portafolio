25340c1dbf574c31e9c395a41f0ae85cc85e1b23
Daniel Villafranqui <danedo20472@gmail.com>
2026-01-05 20:53:30 -0500

    update work experience

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -12,7 +12,7 @@ export default function WorkExperience() {
           role='Desarrollador Web'
           enterprise='Tres media E.I.R.L'
           time='Julio 2024 - Actualidad'
-          description='Participo en el desarrollo de distintos proyectos web, principalmente usando el gestor de contenido de Wordpress en conjunto de plugins como motionPage y BricksForge para animaciones basadas en Gsap, Polylang para manejar diferentes idiomas y con el tema Bricks Builder.'
+          description='Participé en el desarrollo de landing pages, catálogos administrables y e-commerce usando WordPress, WooCommerce y ACF. Implementé animaciones avanzadas con GSAP mediante MotionPage y BricksForge, mejorando la experiencia visual. Construí sitios modulares y escalables con Bricks Builder, optimizando tiempos de entrega. Colaboré con diseñadores y desarrolladores para traducir prototipos de Figma en interfaces funcionales. Además, aporté mejoras técnicas internas y desarrollé módulos en PHP para sistemas de gestión más complejos.'
         />
         <ExperienceCard
           role='Proximamente...'
